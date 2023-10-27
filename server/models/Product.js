291dbb43d20d30a416d511c37b2fd5504944c21d
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-10-27 11:42:14 +0530

    feat : schema model design for products

---
+++ server/models/Product.js
@@ -0,0 +1,31 @@
+import { Schema, model } from "mongoose";
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = model("Product", productSchema);
+
+export default Product;
--- server/models/User.js
+++ server/models/User.js
@@ -1,18 +1,23 @@
 import { Schema, model } from "mongoose";
 
-const userSchema = new Schema({
-  name: { type: String, default: "_" },
+const userSchema = new Schema(
+  {
+    name: { type: String, default: "_" },
 
-  email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
 
-  mobile: { type: Number, unique: true, required: true },
+    mobile: { type: Number, unique: true, required: true },
 
-  password: { type: String },
+    password: { type: String },
 
-  address: { type: String },
+    address: { type: String },
 
-  gender: { type: String, default: "prefer not to say" },
-});
+    gender: { type: String, default: "prefer not to say" },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const User = model("User", userSchema);
 
