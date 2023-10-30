8c2018b9596daa0def6ae2d37cbfac9e8d929e26
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-10-30 22:08:44 +0530

    feat : react router dom added

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./views/Login/Login";
+import Signup from "./views/Signup/Signup";
+import Home from "./views/Home/Home";
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
-   <h1>hie this is saurabh shopee</h1>
-  </>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>
+  },
+  {
+    path: '/login',
+    element: <Login/>
+  },
+  {
+    path: '/signup',
+    element: <Signup/>
+  }
+])
+root.render(<RouterProvider router= {router}/>);
