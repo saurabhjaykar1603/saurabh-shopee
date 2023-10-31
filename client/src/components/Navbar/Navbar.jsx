ae057f0a8d9be960d3818e7ac41ce08a73e46c71
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-10-31 20:00:52 +0530

    feat : login user data read from local storage

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    const getloggedInUser = JSON.parse(localStorage.getItem("user" || "{}"));
+    setUser(getloggedInUser);
+  }, []);
   return (
     <>
       <div className="sticky-top">
         <nav className="navbar navbar-expand-lg navbar-light bg-warning py-3 shadow">
           <div className="container d-flex justify-content-between align-items-center">
             <div>
-              <Link to='/' className="text-deco-none ">
+              <Link to="/" className="text-deco-none ">
                 {" "}
                 <h4 className="fs-4 fw-bold text-dark"> Saurabh's Shopee</h4>
               </Link>
@@ -43,7 +48,7 @@ function Navbar() {
             </div>
 
             <div>
-              <h5 className="pointer login-links">Hello User </h5>
+              <h5 className="pointer login-links"> Hello {user.name || "Guest "}! </h5>
             </div>
           </div>
         </nav>
--- client/src/views/MyOrder/MyOrder.jsx
+++ client/src/views/MyOrder/MyOrder.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 
 function MyOrder() {
- uh
   return (
     <>
       <div className="sticky-top">
